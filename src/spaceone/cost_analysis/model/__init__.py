ab11e47318e704d7355f962dc22b40ca25dc104b
Jongmin Kim <whdalsrnt@megazone.com>
2021-12-21 15:14:19 +0900

    fix: fix import bug

--- src/spaceone/cost_analysis/model/__init__.py
+++ src/spaceone/cost_analysis/model/__init__.py
@@ -2,7 +2,7 @@ from spaceone.cost_analysis.model.data_source_model import DataSource
 from spaceone.cost_analysis.model.data_source_rule_model import DataSourceRule
 from spaceone.cost_analysis.model.schedule_model import Schedule
 from spaceone.cost_analysis.model.exchange_rate_model import ExchangeRate
-from spaceone.cost_analysis.model.cost_model import Cost, AggregatedCost
+from spaceone.cost_analysis.model.cost_model import Cost
 from spaceone.cost_analysis.model.budget_model import Budget
 from spaceone.cost_analysis.model.budget_usage_model import BudgetUsage
 from spaceone.cost_analysis.model.dashboard_model import Dashboard
