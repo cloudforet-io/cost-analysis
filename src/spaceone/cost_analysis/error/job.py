e8e44650f91392b6de38b4fc9236d78e028b498e
Jongmin Kim <whdalsrnt@megazone.com>
2021-12-30 03:39:24 +0900

    feat: implement duplicate sync job cancellation
    (cherry picked from commit 47efbff9cf0fc651f48cfa3828ae239eb7d89b7a)

--- src/spaceone/cost_analysis/error/job.py
+++ src/spaceone/cost_analysis/error/job.py
@@ -3,3 +3,8 @@ from spaceone.core.error import *
 
 class ERROR_JOB_STATE(ERROR_UNKNOWN):
     _message = 'Only running jobs can be canceled. (job_state = {job_state})'
+
+
+class ERROR_DUPLICATE_JOB(ERROR_UNKNOWN):
+    _message = 'The same job is already running. (data_source_id = {data_source_id})'
+
