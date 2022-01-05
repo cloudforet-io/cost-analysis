4fbe673c67f4201875f7161e0d32637a7161fabd
Jongmin Kim <whdalsrnt@megazone.com>
2022-01-05 13:38:44 +0900

    fix: fix authorization bug
    (cherry picked from commit e88a85bf2a1860aa35f3ef3ccaea9e34f097ade5)

--- src/spaceone/cost_analysis/service/budget_usage_service.py
+++ src/spaceone/cost_analysis/service/budget_usage_service.py
@@ -58,7 +58,7 @@ class BudgetUsageService(BaseService):
         }
     })
     @check_required(['query', 'domain_id'])
-    @append_query_filter(['domain_id'])
+    @append_query_filter(['domain_id', 'user_projects', 'user_project_groups'])
     @append_keyword_filter(['budget_id', 'name'])
     def stat(self, params):
         """
