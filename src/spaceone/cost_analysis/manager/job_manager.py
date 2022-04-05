96ed07188400517e1edcd6c8434ef4e10f1b4a02
Jongmin Kim <whdalsrnt@megazone.com>
2022-04-05 13:39:52 +0900

    fix: fix argument bug when calling create_job func
    (cherry picked from commit 1f6e4e56eb94495fd26e1404a6f7a2425448f54c)

--- src/spaceone/cost_analysis/manager/job_manager.py
+++ src/spaceone/cost_analysis/manager/job_manager.py
@@ -34,6 +34,9 @@ class JobManager(BaseManager):
             return False
 
     def create_job(self, data_source_id, domain_id, job_options, total_tasks, changed=None):
+        job_options['no_preload_cache'] = job_options.get('no_preload_cache', False)
+        job_options['start'] = job_options.get('start')
+
         data = {
             'data_source_id': data_source_id,
             'domain_id': domain_id,
