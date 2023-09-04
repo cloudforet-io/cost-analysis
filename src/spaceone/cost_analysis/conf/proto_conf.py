16dc5a78cb5f27602246665e63b0f59ea602054e
Jongmin Kim <whdalsrnt@megazone.com>
2023-09-04 19:56:16 +0900

    feat: delete deprecated codes

--- src/spaceone/cost_analysis/conf/proto_conf.py
+++ src/spaceone/cost_analysis/conf/proto_conf.py
@@ -1,15 +1,10 @@
 PROTO = {
     'spaceone.cost_analysis.interface.grpc.v1.data_source': ['DataSource'],
-    'spaceone.cost_analysis.interface.grpc.v1.schedule': ['Schedule'],
-    'spaceone.cost_analysis.interface.grpc.v1.exchange_rate': ['ExchangeRate'],
     'spaceone.cost_analysis.interface.grpc.v1.cost': ['Cost'],
     'spaceone.cost_analysis.interface.grpc.v1.data_source_rule': ['DataSourceRule'],
     'spaceone.cost_analysis.interface.grpc.v1.budget': ['Budget'],
     'spaceone.cost_analysis.interface.grpc.v1.budget_usage': ['BudgetUsage'],
-    'spaceone.cost_analysis.interface.grpc.v1.public_dashboard': ['PublicDashboard'],
-    'spaceone.cost_analysis.interface.grpc.v1.user_dashboard': ['UserDashboard'],
     'spaceone.cost_analysis.interface.grpc.v1.cost_query_set': ['CostQuerySet'],
-    'spaceone.cost_analysis.interface.grpc.v1.custom_widget': ['CustomWidget'],
     'spaceone.cost_analysis.interface.grpc.v1.job': ['Job'],
     'spaceone.cost_analysis.interface.grpc.v1.job_task': ['JobTask'],
 }
--- src/spaceone/cost_analysis/error/__init__.py
+++ src/spaceone/cost_analysis/error/__init__.py
@@ -1,7 +1,6 @@
 from spaceone.cost_analysis.error.data_source import *
 from spaceone.cost_analysis.error.cost import *
 from spaceone.cost_analysis.error.budget import *
-from spaceone.cost_analysis.error.exchange_rate import *
 from spaceone.cost_analysis.error.job import *
 from spaceone.cost_analysis.error.job_task import *
 from spaceone.cost_analysis.error.data_source_rule import *
