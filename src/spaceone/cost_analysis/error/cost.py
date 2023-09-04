1b496b465324fd25c8bc33a8c9ad2c6e98310495
Jongmin Kim <whdalsrnt@megazone.com>
2023-09-04 20:01:30 +0900

    refactor: refactor Cost APIs

--- src/spaceone/cost_analysis/error/cost.py
+++ src/spaceone/cost_analysis/error/cost.py
@@ -3,3 +3,7 @@ from spaceone.core.error import *
 
 class ERROR_INVALID_DATE_RANGE(ERROR_INVALID_ARGUMENT):
     _message = '{reason}'
+
+
+class ERROR_NOT_SUPPORT_QUERY_OPTION(ERROR_INVALID_ARGUMENT):
+    _message = 'Not support query option. (query_option = {query_option})'
--- src/spaceone/cost_analysis/model/cost_model.py
+++ src/spaceone/cost_analysis/model/cost_model.py
@@ -5,19 +5,14 @@ from spaceone.core.model.mongo_model import MongoModel
 
 class Cost(MongoModel):
     cost_id = StringField(max_length=40, generate_id='cost')
-    usd_cost = FloatField(required=True)
-    original_currency = StringField(max_length=40)
-    original_cost = FloatField(required=True)
+    cost = FloatField(required=True)
     usage_quantity = FloatField(default=0)
+    usage_unit = StringField(max_length=255, default=None, null=True)
     provider = StringField(max_length=40, default=None, null=True)
     region_code = StringField(max_length=255, default=None, null=True)
     region_key = StringField(max_length=255, default=None, null=True)
-    category = StringField(max_length=255, default=None, null=True)
     product = StringField(max_length=255, default=None, null=True)
-    account = StringField(max_length=255, default=None, null=True)
     usage_type = StringField(max_length=255, default=None, null=True)
-    usage_unit = StringField(max_length=255, default=None, null=True)
-    resource_group = StringField(default=None, null=True)
     resource = StringField(default=None, null=True)
     tags = DictField(default={})
     additional_info = DictField(default={})
@@ -25,44 +20,35 @@ class Cost(MongoModel):
     project_id = StringField(max_length=40, default=None, null=True)
     data_source_id = StringField(max_length=40)
     job_id = StringField(max_length=40, default=None, null=True)
+    job_task_id = StringField(max_length=40, default=None, null=True)
     domain_id = StringField(max_length=40)
-    created_at = DateTimeField(auto_now_add=True)
-    billed_at = DateTimeField(required=True)
-    billed_year = StringField(max_length=20)
-    billed_month = StringField(max_length=20)
-    billed_date = StringField(max_length=20)
+    billed_year = StringField(max_length=4, required=True)
+    billed_month = StringField(max_length=7, required=True)
+    billed_date = StringField(max_length=10, required=True)
 
     meta = {
         'updatable_fields': [],
         'minimal_fields': [
             'cost_id',
-            'usd_cost',
+            'cost',
             'provider',
             'region_code',
-            'category',
             'product',
-            'account',
             'usage_type',
-            'resource_group',
             'resource',
             'data_source_id',
-            'billed_at'
+            'billed_date'
         ],
         'change_query_keys': {
             'user_projects': 'project_id'
         },
         'indexes': [
             {
-                "fields": ['domain_id', 'data_source_id', '-billed_month', 'account'],
-                "name": "COMPOUND_INDEX_FOR_SYNC_JOB_1"
+                "fields": ['domain_id', 'data_source_id', 'job_id', 'job_task_id', '-billed_month'],
+                "name": "COMPOUND_INDEX_FOR_SYNC_JOB"
             },
             {
-                "fields": ['domain_id', 'data_source_id', 'job_id', '-billed_at', 'account'],
-                "name": "COMPOUND_INDEX_FOR_SYNC_JOB_2"
-            },
-            {
-                "fields": ['domain_id', '-billed_date', 'project_id', 'provider', 'service_account_id',
-                           'usd_cost', 'usage_quantity'],
+                "fields": ['domain_id', 'data_source_id', '-billed_date', 'project_id', 'cost'],
                 "name": "COMPOUND_INDEX_FOR_SEARCH"
             },
             {
@@ -74,17 +60,14 @@ class Cost(MongoModel):
 
 
 class MonthlyCost(MongoModel):
-    usd_cost = FloatField(required=True)
+    cost = FloatField(required=True)
     usage_quantity = FloatField(default=0)
+    usage_unit = StringField(max_length=255, default=None, null=True)
     provider = StringField(max_length=40, default=None, null=True)
     region_code = StringField(max_length=255, default=None, null=True)
     region_key = StringField(max_length=255, default=None, null=True)
-    category = StringField(max_length=255, default=None, null=True)
     product = StringField(max_length=255, default=None, null=True)
-    account = StringField(max_length=255, default=None, null=True)
     usage_type = StringField(max_length=255, default=None, null=True)
-    usage_unit = StringField(max_length=255, default=None, null=True)
-    resource_group = StringField(default=None, null=True)
     resource = StringField(default=None, null=True)
     tags = DictField(default={})
     additional_info = DictField(default={})
@@ -92,10 +75,10 @@ class MonthlyCost(MongoModel):
     project_id = StringField(max_length=40, default=None, null=True)
     data_source_id = StringField(max_length=40)
     job_id = StringField(max_length=40, default=None, null=True)
+    job_task_id = StringField(max_length=40, default=None, null=True)
     domain_id = StringField(max_length=40)
-    created_at = DateTimeField(auto_now_add=True)
-    billed_year = StringField(max_length=20)
-    billed_month = StringField(max_length=20)
+    billed_year = StringField(max_length=4, required=True)
+    billed_month = StringField(max_length=7, required=True)
 
     meta = {
         'updatable_fields': [],
@@ -104,13 +87,16 @@ class MonthlyCost(MongoModel):
         },
         'indexes': [
             {
-                "fields": ['domain_id', 'data_source_id', 'job_id', '-billed_month'],
+                "fields": ['domain_id', 'data_source_id', 'job_id', 'job_task_id', '-billed_month'],
                 "name": "COMPOUND_INDEX_FOR_SYNC_JOB"
             },
             {
-                "fields": ['domain_id', '-billed_month', 'project_id', 'provider', 'service_account_id',
-                           'usd_cost', 'usage_quantity'],
-                "name": "COMPOUND_INDEX_FOR_SEARCH"
+                "fields": ['domain_id', 'data_source_id', '-billed_month', 'project_id', 'cost'],
+                "name": "COMPOUND_INDEX_FOR_SEARCH_1"
+            },
+            {
+                "fields": ['domain_id', 'data_source_id', '-billed_year', 'project_id', 'cost'],
+                "name": "COMPOUND_INDEX_FOR_SEARCH_2"
             },
         ]
     }
@@ -119,21 +105,17 @@ class MonthlyCost(MongoModel):
 class CostQueryHistory(MongoModel):
     query_hash = StringField(max_length=255)
     query_options = DictField(default={})
+    data_source_id = StringField(max_length=40)
     domain_id = StringField(max_length=40)
-    granularity = StringField(max_length=40, default=None, null=True)
-    start = DateField(defulat=None, null=True)
-    end = DateField(default=None, null=True)
     updated_at = DateTimeField(auto_now=True)
 
     meta = {
         'updatable_fields': [
-            'start',
-            'end',
             'updated_at'
         ],
         'indexes': [
             {
-                "fields": ['domain_id', 'query_hash'],
+                "fields": ['domain_id', 'domain_id', 'query_hash'],
                 "name": "COMPOUND_INDEX_FOR_SEARCH"
             },
         ]
