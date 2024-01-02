8884cebb33c84df92c1556bba9dfc5785c179ee1
ImMin5 <mino@megazone.com>
2024-01-03 01:44:22 +0900

    feat: modify cost_query_set apis

--- src/spaceone/cost_analysis/manager/cost_query_set_manager.py
+++ src/spaceone/cost_analysis/manager/cost_query_set_manager.py
@@ -11,14 +11,14 @@ class CostQuerySetManager(BaseManager):
         super().__init__(*args, **kwargs)
         self.cost_query_set_model: CostQuerySet = self.locator.get_model("CostQuerySet")
 
-    def create_cost_query_set(self, params):
-        def _rollback(cost_query_set_vo):
+    def create_cost_query_set(self, params: dict):
+        def _rollback(vo: CostQuerySet):
             _LOGGER.info(
                 f"[create_cost_query_set._rollback] "
                 f"Delete cost_query_set : {cost_query_set_vo.name} "
-                f"({cost_query_set_vo.cost_query_set_id})"
+                f"({vo.cost_query_set_id})"
             )
-            cost_query_set_vo.delete()
+            vo.delete()
 
         cost_query_set_vo: CostQuerySet = self.cost_query_set_model.create(params)
         self.transaction.add_rollback(_rollback, cost_query_set_vo)
@@ -42,16 +42,25 @@ class CostQuerySetManager(BaseManager):
         self.transaction.add_rollback(_rollback, cost_query_set_vo.to_dict())
         return cost_query_set_vo.update(params)
 
-    def delete_cost_query_set(self, cost_query_set_id, domain_id):
-        cost_query_set_vo: CostQuerySet = self.get_cost_query_set(
-            cost_query_set_id, domain_id
-        )
+    @staticmethod
+    def delete_cost_query_set_by_vo(cost_query_set_vo: CostQuerySet) -> None:
         cost_query_set_vo.delete()
 
-    def get_cost_query_set(self, cost_query_set_id, user_id, domain_id):
-        return self.cost_query_set_model.get(
-            cost_query_set_id=cost_query_set_id, user_id=user_id, domain_id=domain_id
-        )
+    def get_cost_query_set(
+        self,
+        cost_query_set_id: str,
+        user_id: str,
+        domain_id: str,
+        workspace_id: str = None,
+    ) -> CostQuerySet:
+        conditions = {
+            "cost_query_set_id": cost_query_set_id,
+            "user_id": user_id,
+            "domain_id": domain_id,
+        }
+        if workspace_id:
+            conditions["workspace_id"] = workspace_id
+        return self.cost_query_set_model.get(**conditions)
 
     def list_cost_query_sets(self, query={}):
         return self.cost_query_set_model.query(**query)
--- src/spaceone/cost_analysis/model/cost_query_set_model.py
+++ src/spaceone/cost_analysis/model/cost_query_set_model.py
@@ -4,35 +4,26 @@ from spaceone.core.model.mongo_model import MongoModel
 
 
 class CostQuerySet(MongoModel):
-    cost_query_set_id = StringField(max_length=40, generate_id='query', unique=True)
+    cost_query_set_id = StringField(max_length=40, generate_id="query", unique=True)
     name = StringField(max_length=255)
     options = DictField(default={})
     tags = DictField(default={})
     user_id = StringField(max_length=40)
     data_source_id = StringField(max_length=40)
+    workspace_id = StringField(max_length=40, default=None, null=True)
     domain_id = StringField(max_length=40)
     created_at = DateTimeField(auto_now_add=True)
     updated_at = DateTimeField(auto_now=True)
 
     meta = {
-        'updatable_fields': [
-            'name',
-            'options',
-            'tags'
+        "updatable_fields": ["name", "options", "tags"],
+        "minimal_fields": [
+            "cost_query_set_id",
+            "name",
+            "user_id",
+            "data_source_id",
+            "workspace_id",
         ],
-        'minimal_fields': [
-            'cost_query_set_id',
-            'name',
-            'user_id',
-            'data_source_id'
-        ],
-        'ordering': [
-            'name'
-        ],
-        'indexes': [
-            'name',
-            'user_id',
-            'data_source_id',
-            'domain_id'
-        ]
+        "ordering": ["name"],
+        "indexes": ["name", "user_id", "data_source_id", "domain_id"],
     }
