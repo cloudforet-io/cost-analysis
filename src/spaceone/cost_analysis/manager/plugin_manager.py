1afde573acff722c0127a070d5c7829d234674a3
Jongmin Kim <whdalsrnt@megazone.com>
2024-01-07 08:24:57 +0900

    fix: change wrong system token

--- src/spaceone/cost_analysis/manager/plugin_manager.py
+++ src/spaceone/cost_analysis/manager/plugin_manager.py
@@ -1,5 +1,6 @@
 import logging
 
+from spaceone.core import config
 from spaceone.core.manager import BaseManager
 from spaceone.core.connector.space_connector import SpaceConnector
 
@@ -14,7 +15,7 @@ class PluginManager(BaseManager):
         )
 
     def get_plugin_endpoint(self, plugin_info, domain_id):
-        system_token = self.transaction.get_meta("token")
+        system_token = config.get_global("TOKEN")
 
         response = self.plugin_connector.dispatch(
             "Plugin.get_plugin_endpoint",
