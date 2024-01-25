e2c89134db602500409ff62a839da4fd00b1a972
ImMin5 <mino@megazone.com>
2024-01-26 01:06:45 +0900

    feat: modify aggregate cost_report api

--- src/spaceone/cost_analysis/interface/grpc/cost_report.py
+++ src/spaceone/cost_analysis/interface/grpc/cost_report.py
@@ -13,3 +13,27 @@ class CostReport(BaseAPI, cost_report_pb2_grpc.CostReportServicer):
         cost_report_svc = CostReportService(metadata)
         cost_report_svc.send(params)
         return self.empty()
+
+    def get_url(self, request, context):
+        params, metadata = self.parse_request(request, context)
+        cost_report_svc = CostReportService(metadata)
+        response: dict = cost_report_svc.get_url(params)
+        return self.dict_to_message(response)
+
+    def get(self, request, context):
+        params, metadata = self.parse_request(request, context)
+        cost_report_svc = CostReportService(metadata)
+        response: dict = cost_report_svc.get(params)
+        return self.dict_to_message(response)
+
+    def list(self, request, context):
+        params, metadata = self.parse_request(request, context)
+        cost_report_svc = CostReportService(metadata)
+        response: dict = cost_report_svc.list(params)
+        return self.dict_to_message(response)
+
+    def stat(self, request, context):
+        params, metadata = self.parse_request(request, context)
+        cost_report_svc = CostReportService(metadata)
+        response: dict = cost_report_svc.stat(params)
+        return self.dict_to_message(response)
