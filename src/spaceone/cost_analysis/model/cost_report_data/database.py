ed894c95b98bb97ccf41512d9f46e8782e989189
ImMin5 <mino@megazone.com>
2024-06-21 16:12:54 +0900

    feat: add Workspace Member permissions for cost report data analyze
    Signed-off-by: ImMin5 <mino@megazone.com>

--- src/spaceone/cost_analysis/model/cost_report_data/database.py
+++ src/spaceone/cost_analysis/model/cost_report_data/database.py
@@ -21,9 +21,8 @@ class CostReportData(MongoModel):
     data_source_id = StringField(max_length=40)
     cost_report_id = StringField(max_length=40)
     cost_report_config_id = StringField(max_length=40)
-    workspace_id = StringField(
-        max_length=40, default=None, null=True
-    )  # todo workspace_id required
+    project_id = StringField(max_length=40, default=None, null=True)
+    workspace_id = StringField(max_length=40)
     domain_id = StringField(max_length=40)
     created_at = DateTimeField(auto_now_add=True)
 
@@ -36,9 +35,19 @@ class CostReportData(MongoModel):
             "workspace_id",
         ],
         "ordering": ["is_confirmed", "-report_year"],
+        "change_query_keys": {"user_projects": "project_id"},
         "indexes": [
-            "cost_report_config_id",
-            "cost_report_id",
-            "domain_id",
+            {
+                "fields": [
+                    "cost_report_config_id",
+                    "cost_report_id",
+                    "domain_id",
+                ],
+                "name": "COMPOUND_INDEX_FOR_DEFAULT_COST_REPORT_DATA",
+            },
+            {
+                "fields": ["is_confirmed", "domain_id", "workspace_id", "project_id"],
+                "name": "COMPOUND_INDEX_FOR_WORKSPACE_LANDING",
+            },
         ],
     }
