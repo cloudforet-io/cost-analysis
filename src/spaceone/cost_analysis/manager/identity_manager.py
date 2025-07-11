f22e428d2114ecceadb6636bf8f4dadf40c5cd9d
jinyoungmoonDEV <97270590+jinyoungmoonDEV@users.noreply.github.com>
2025-07-11 12:38:02 +0900

    Update identity_manager.py
    Signed-off-by: jinyoungmoonDEV <97270590+jinyoungmoonDEV@users.noreply.github.com>

--- src/spaceone/cost_analysis/manager/identity_manager.py
+++ src/spaceone/cost_analysis/manager/identity_manager.py
@@ -120,10 +120,10 @@ class IdentityManager(BaseManager):
         else:
             return self.identity_conn.dispatch("Workspace.list", params)
 
-    def list_workspace_users(self, params: dict, domain_id: str, token: str = None) -> dict:
-        if self.token_type == "SYSTEM_TOKEN" or token:
+    def list_workspace_users(self, params: dict, domain_id: str) -> dict:
+        if self.token_type == "SYSTEM_TOKEN":
             return self.identity_conn.dispatch(
-                "WorkspaceUser.list", params, x_domain_id=domain_id, token=token
+                "WorkspaceUser.list", params, x_domain_id=domain_id
             )
         else:
             return self.identity_conn.dispatch("WorkspaceUser.list", params)
